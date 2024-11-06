7b71e304cb365295d1354e5e4abe3b203250e1e8
Gustavo <gustavopma90@gmail.com>
2024-11-06 14:09:48 -0300

    adicionando componentes da busca

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,11 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <section className="pink_container">
@@ -8,6 +15,12 @@ export default function Home() {
         </h1>
         <p className="sub-heading !max-w-3x1">
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}""` : "All Startups"}
         </p>
       </section>
     </>
